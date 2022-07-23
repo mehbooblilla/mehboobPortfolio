5ab766a7d994cf020a3b875f9d36bc66cd6b2577
Muhammad Mehboob <mehbooblilla007@gmail.com>
2022-07-24 00:59:00 +0500

    moddfy some file

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -12,49 +12,37 @@ function Portfolio() {
                     <h1>Project</h1>
                 </div>
             </header>
-            <div className='components container' >
+            <div className='row cardComponent '>
 
-                <div className='row cardComponent '>
-                    <div className='col-4'>
-                        <Link className='link' to="/PizzaProject" >
-                            <div className="card hotelCard" >
-                                <div className='overflow'>
-                                    <img src={pizzaImg} className="card-img" alt="..." />
-                                </div>
-                                <div class="card-body">
-                                    <h3>Pizza Shop</h3>
-                                </div>
-                            </div>
-                        </Link>
-                    </div>
-                    <div className='col-4'>
-                        <Link className='link' to="/GymProject" >
-                            <div className="card hotelCard" >
-
-                                <div className='overflow'>
-                                    <img src={gymImg} className="card-img" alt="..." />
-                                </div>
-                                <div class="card-body">
-                                    <h3>Gym Website</h3>
-                                </div>
-
-                            </div>
-                        </Link>
-                    </div>
-                    <div className='col-4'>
-                        <Link className='link' to="/FypProject" >
-                            <div className="card hotelCard" >
-
-                                <div className='overflow'>
-                                    <img src={fypImg} className="card-img" alt="..." />
-                                </div>
-                                <div class="card-body">
-                                    <h3>Tourism website (FYP)</h3>
-                                </div>
-
-                            </div>
-                        </Link>
-                    </div>
+                <div className="card hotelCard" style={{ width: "18rem" }} >
+                    <Link to="/PizzaProject" className='link'>
+                        <div className='overflow'>
+                            <img src={pizzaImg} className="card-img" alt="..." />
+                        </div>
+                        <div class="card-body">
+                            <h3>Hotels</h3>
+                        </div>
+                    </Link>
+                </div>
+                <div className="card hotelCard" style={{ width: "18rem" }} >
+                    <Link to="/GymProject" className='link' >
+                        <div className='overflow'>
+                            <img src={gymImg} className="card-img" alt="..." />
+                        </div>
+                        <div class="card-body">
+                            <h3>Townhouses</h3>
+                        </div>
+                    </Link>
+                </div>
+                <div className="card hotelCard" style={{ width: "18rem" }} >
+                    <Link to="/FypProject" className='link'>
+                        <div className='overflow'>
+                            <img src={fypImg} className="card-img" alt="..." />
+                        </div>
+                        <div class="card-body">
+                            <h3>Guest Houses</h3>
+                        </div>
+                    </Link>
                 </div>
             </div>
         </>
