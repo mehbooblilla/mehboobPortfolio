3abff5365155bf2d7b3f27e27fd7de910e423410
Muhammad Mehboob <mehbooblilla007@gmail.com>
2022-08-05 09:06:05 +0500

    add file

--- src/App.js
+++ src/App.js
@@ -1,27 +1,28 @@
 import NavbarComponent from "./components/Navbar";
 import Home from "./pages/Home";
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap/dist/js/bootstrap.js";
-import About from './pages/About';
-import Contact from './pages/Contact';
-import Portfolio from './pages/Portfolio';
-import GymProject from './pages/GymProject';
-import PizzaProject from './pages/PizzaProject';
-import FypProject from './pages/FypProject';
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Portfolio from "./pages/Portfolio";
+import GymProject from "./pages/GymProject";
+import PizzaProject from "./pages/PizzaProject";
+import FypProject from "./pages/FypProject";
 function App() {
   return (
     <>
       <BrowserRouter>
         <NavbarComponent />
         <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/About' element={<About />} />
-          <Route path='/Contact' element={<Contact />} />
-          <Route path='/Portfolio' element={<Portfolio />} />
-          <Route path='/GymProject' element={<GymProject />} />
-          <Route path='/PizzaProject' element={<PizzaProject />} />
-          <Route path='/FypProject' element={<FypProject />} />
+          <Route path="/" element={<Home />} />
+          <Route index element={<Home />} />
+          <Route path="/About" element={<About />} />
+          <Route path="/Contact" element={<Contact />} />
+          <Route path="/Portfolio" element={<Portfolio />} />
+          <Route path="/GymProject" element={<GymProject />} />
+          <Route path="/PizzaProject" element={<PizzaProject />} />
+          <Route path="/FypProject" element={<FypProject />} />
         </Routes>
       </BrowserRouter>
     </>
